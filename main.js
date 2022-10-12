0fd941219ceedeb31ddd804484a5e875b3a7d981
devindersinghnagi <98323355+devindersinghnagi@users.noreply.github.com>
2022-10-12 05:03:21 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+noseX =0;
+noseY =0;
+function setup()
+
+{
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+console.log('poseNet is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.Y;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+}
+
+function draw()
+{
+image(video,0 ,0 ,300 ,300 )
+image(clown_nose, noseX, noseY,30,30);
+}
+
+function take_snapshot(){
+    save("myMustachFilter.png");
+}
+function preload(){
+    clown_nose = loadImage('imageedit_1_4101613616.png');
+}
+
+
+
+
